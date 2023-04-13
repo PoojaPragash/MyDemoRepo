aa737d6ac8350aac9fa8c6a8c6404402d0f3f4e1
Pooja Pragash <pooja.pragash@ibm.com>
2023-04-13 15:12:01 +0530

    Added registeration feature

--- A.java
+++ A.java
@@ -2,6 +2,6 @@ public class A
 {
 	public static void main(String args[])
 	{
-		System.out.println("great day ahead");
+		System.out.println("day ahead");
 	}
 }
---
+++ B.java
@@ -0,0 +1,7 @@
+public class B
+{
+	pulic static void main(String args[])
+	{
+		System.out.println("hi");
+	}
+}
